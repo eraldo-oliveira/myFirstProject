2f159dc9b6adaa9bf66b00066b5bdd4820d19ab2
eraldo-oliveira <eraldo.oliveira.junior@gmail.com>
2024-11-13 21:19:49 +0000

    add tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import Home from './views/HomeView.jsx'
+import "./style/global.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
