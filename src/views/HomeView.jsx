7d0f4cea64114a231b4433484af63f8146cdac96
eraldo-oliveira <eraldo.oliveira.junior@gmail.com>
2024-11-13 20:37:23 +0000

    init

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,41 @@
+
+
+function HomeView() {
+  return (
+    <>
+      <nav>
+        <img src="" alt="[]" class="logo"/>
+        <span class="site-name">Nome do Site</span>
+        <ul class="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="add-location">Add Location</a></li>
+            <li><a href="the-traveler">The Traveler</a></li>
+        </ul>
+      </nav>
+
+      <main>
+        <h2>Eu sou a Homepage</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
+          itaque, tempora, corporis quas voluptatibus repellat provident sint et
+          alias incidunt officiis ipsam molestias tempore! Maxime quaerat alias
+          nesciunt esse.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
+          itaque, tempora, corporis quas voluptatibus repellat provident sint et
+          alias incidunt officiis ipsam molestias tempore! Maxime quaerat alias
+          nesciunt esse.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
+          itaque, tempora, corporis quas voluptatibus repellat provident sint et
+          alias incidunt officiis ipsam molestias tempore! Maxime quaerat alias
+          nesciunt esse.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default HomeView;
