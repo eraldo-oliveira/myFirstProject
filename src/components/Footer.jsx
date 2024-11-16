3f9e37e531d289f0353677d9d5f1ee53c519c776
eraldo-oliveira <eraldo.oliveira.junior@gmail.com>
2024-11-16 17:10:55 +0000

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@
 function Footer() {
     return (
       <footer className="bg-yellow-300 p-6 mt-auto">
-        <div className="flex items-center">
-          <ul className="flex gap-10 md:hidden">
+        <div className="flex justify-center">
+          <ul className="flex flex-col items-center gap-4 md:hidden">
             <li className="hover:text-red-500"><a href="/">Home</a></li>
             <li className="hover:text-red-500"><a href="/about">Add Location</a></li>
             <li className="hover:text-red-500"><a href="/contact">The Traveler</a></li>
